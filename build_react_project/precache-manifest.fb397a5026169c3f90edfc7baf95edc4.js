3bdf6ef917795bcff710b4431147c67d6665d2bf
mikhailmibo <amelis.com.ua@gmail.com>
2018-11-13 18:03:48 +0200

    react_project

---
+++ build_react_project/precache-manifest.fb397a5026169c3f90edfc7baf95edc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4a2dcf61becdc135543",
+    "url": "/static/js/main.b4a2dcf6.chunk.js"
+  },
+  {
+    "revision": "319dbcb40bbf7ec1b843",
+    "url": "/static/js/1.319dbcb4.chunk.js"
+  },
+  {
+    "revision": "b4a2dcf61becdc135543",
+    "url": "/static/css/main.80ffbd8b.chunk.css"
+  },
+  {
+    "revision": "319dbcb40bbf7ec1b843",
+    "url": "/static/css/1.bfcfbfc5.chunk.css"
+  },
+  {
+    "revision": "1faea97f3c0c8205636b578cdbb4dbe2",
+    "url": "/index.html"
+  }
+];
